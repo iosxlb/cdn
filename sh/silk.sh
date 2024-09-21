710793e923fdd0502d49d53f3d61d62235aeebca
iOS小萝卜 <108018198+iosxlb@users.noreply.github.com>
2024-09-21 15:20:10 +0800

    Add files via upload

--- sh/silk.sh
+++ sh/silk.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
-yum install gcc -y
-yum install epel-release -y
-rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
-rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
-yum install ffmpeg -y
-yum install wget -y 
-yum install make -y
-yum install gcc-c++ -y
 cd opt/
-git clone https://github.com/iosxlb/silk-v3-decoder.git
+git clone https://github.com/iosxlb/silk-v3-encoder.git
 cd silk-v3-decoder
-chmod +x converter.sh
-./converter.sh
+chmod 777 silk-v3-decoder
+cd silk-v3-decode/silk
+make encoder
+make decoder
 cp encoder /usr/local/bin/
 chmod 777 /usr/local/bin/encoder
