7b32d1424e5419406b5fddce80c53d60b2237963
iOS小萝卜 <108018198+iosxlb@users.noreply.github.com>
2024-09-22 19:59:46 +0800

    Add files via upload

--- sh/silk.sh
+++ sh/silk.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-
-cd /opt
-
 git clone https://github.com/iosxlb/silk-v3-decoder.git
-
 cd silk-v3-decoder
-
 chmod 777 silk-v3-decoder
-
 sh converter.sh
-
 cd silk
-
 make encoder
-
 make decoder
-
 cp encoder /usr/local/bin/
-
 chmod 777 /usr/local/bin/encoder
-
