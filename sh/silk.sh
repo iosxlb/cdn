fcea34db9a6b0f96c809550e670d2a623664de9f
iOS小萝卜 <108018198+iosxlb@users.noreply.github.com>
2024-09-22 18:40:48 +0800

    Update silk.sh

--- sh/silk.sh
+++ sh/silk.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-
-set -e  # 遇到错误时退出
-
 cd /opt
-
-# 克隆仓库
-if ! git clone https://github.com/iosxlb/silk-v3-decoder.git; then
-    echo "克隆仓库失败"
-    exit 1
-fi
-
+git clone https://github.com/iosxlb/silk-v3-decoder.git
 cd silk-v3-decoder
-
-# 设置权限
-chmod 755 silk-v3-decoder
-
-# 执行 converter.sh
-if [[ -x converter.sh ]]; then
-    ./converter.sh
-else
-    echo "converter.sh 不可执行或未找到"
-    exit 1
-fi
-
+chmod 777 silk-v3-decoder
+sh converter.sh
 cd silk
-
-# 编译
 make encoder
 make decoder
-
-# 复制并设置权限
 cp encoder /usr/local/bin/
-chmod 755 /usr/local/bin/encoder
+chmod 777 /usr/local/bin/encoder
